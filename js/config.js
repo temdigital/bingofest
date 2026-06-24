c3ad77974349b33cd15e260e3fea3b832c4d2505
temdigital <temnoentornosul@gmail.com>
2026-06-24 14:37:53 -0300

    feat: carregar estilos da cartela interativa

--- js/config.js
+++ js/config.js
@@ -3,13 +3,19 @@
  * Nunca use a service_role key no navegador.
  */
 (() => {
-  if (!document.querySelector('link[data-bingo-fixes]')) {
-    const link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.href = 'css/fixes.css?v=20260624-2';
-    link.dataset.bingoFixes = 'true';
+  const styles = [
+    ["bingoFixes", "css/fixes.css?v=20260624-2"],
+    ["bingoManualGame", "css/manual-game.css?v=20260624-1"]
+  ];
+
+  styles.forEach(([datasetKey, href]) => {
+    if (document.querySelector(`link[data-${datasetKey.replace(/[A-Z]/g, letter => `-${letter.toLowerCase()}`)}]`)) return;
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = href;
+    link.dataset[datasetKey] = "true";
     document.head.append(link);
-  }
+  });
 })();
 
 window.BINGO_FEST_CONFIG = Object.freeze({
